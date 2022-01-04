f288e302e1a99998412a68193ab461341954acbc
Breania Harvey <harvey.breania@gmail.com>
2022-01-03 22:34:00 -0600

    home, productList and Detail updated to use GlobalStore

---
+++ client/src/utils/actions.js
@@ -0,0 +1,7 @@
+// used by the ProductList component; store the data retrieved for products
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+// list of categories retrieved from the server
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+// connecting piece; select category from UPDATE_CATEGORIES;
+// display products for that category from UPDATE_PRODUCTS
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
